0d23032edc4a24cda2af021345ad6a38c066bd4e
Max Kuhmichel <kuhmichel.max@fh-swf.de>
2022-09-06 09:30:15 +0200

    added placement constraint. #patch

--- jupyterhub/jupyterhub_config.py
+++ jupyterhub/jupyterhub_config.py
@@ -40,7 +40,7 @@ if os.environ['JUPYTERHUB_SPAWNERCLASS'] == "dockerspawner.SwarmSpawner":
     network_name = os.environ['DOCKER_NETWORK_NAME']
     c.SwarmSpawner.network_name = network_name
     c.SwarmSpawner.extra_host_config = {'network_mode': network_name}
-
+    c.SwarmSpawner.extra_placement_spec = { 'constraints' : ['node.role==worker'] }
 
 elif  os.environ['JUPYTERHUB_SPAWNERCLASS'] == 'dockerspawner.DockerSpawner':
     c.DockerSpawner.network_name = os.environ['DOCKER_NETWORK_NAME']
@@ -53,6 +53,8 @@ elif  os.environ['JUPYTERHUB_SPAWNERCLASS'] == 'modules.CustomSpawner.CustomSpaw
     network_name = os.environ['DOCKER_NETWORK_NAME']
     c.Spawner.network_name = network_name
     c.Spawner.extra_host_config = {'network_mode': network_name}
+    c.SwarmSpawner.extra_placement_spec = { 'constraints' : ['node.role==worker'] } #TODO replace with node.label.gpu
+
 else:
     raise Exception("illegal spawner class found in config {}".format(os.environ['JUPYTERHUB_SPAWNERCLASS']))
 
@@ -73,33 +75,38 @@ c.JupyterHub.shutdown_on_logout = True
 # -> https://github.com/jupyterhub/dockerspawner#data-persistence-and-dockerspawner
 notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
+
 #c.DockerSpawner.volumes = {'jupyterhub-user-{username}': notebook_dir}
 #c.Spawner.env_keep = ['LD_LIBRARY_PATH'] # set in DOCKERFILE of spawned container 
-def cumpute_env_value(spwner_instance):
-    print("==================0envcall=============")
-    logging.warning("==================0envcall=============")
-    print(spwner_instance.__dict__)
-    logging.warning(spwner_instance.__dict__)
-    return "1"
 
-c.Spawner.env_keep = ["NVIDIA_VISIBLE_DEVICES"]
-c.Spawner.environment = {
-    'NB_USER': "${JUPYTERHUB_USER}", 
-    'CHOWN_HOME': 'yes',
-    "NVIDIA_VISIBLE_DEVICES": cumpute_env_value
-    }
-c.SwarmSpawner.env_keep = ["NVIDIA_VISIBLE_DEVICES"]
-c.SwarmSpawner.environment = {
-    'NB_USER': "${JUPYTERHUB_USER}", 
-    'CHOWN_HOME': 'yes',
-    "NVIDIA_VISIBLE_DEVICES": cumpute_env_value
-    }
-c.DockerSpawner.env_keep = ["NVIDIA_VISIBLE_DEVICES"]
-c.DockerSpawner.environment = {
-    'NB_USER': "${JUPYTERHUB_USER}", 
-    'CHOWN_HOME': 'yes',
-    "NVIDIA_VISIBLE_DEVICES": cumpute_env_value
-    }
+
+#def cumpute_env_value(spwner_instance):
+#    print("==================0envcall=============")
+#    logging.warning("==================0envcall=============")
+#    print(spwner_instance.__dict__)
+#    logging.warning(spwner_instance.__dict__)
+#    return "1"
+
+
+# theese dont do anything aparently, have have to use our own spawner
+#c.Spawner.env_keep = ["NVIDIA_VISIBLE_DEVICES"]
+#c.Spawner.environment = {
+#    'NB_USER': "${JUPYTERHUB_USER}", 
+#    'CHOWN_HOME': 'yes',
+#    "NVIDIA_VISIBLE_DEVICES": cumpute_env_value
+#    }
+#c.SwarmSpawner.env_keep = ["NVIDIA_VISIBLE_DEVICES"]
+#c.SwarmSpawner.environment = {
+#    'NB_USER': "${JUPYTERHUB_USER}", 
+#    'CHOWN_HOME': 'yes',
+#    "NVIDIA_VISIBLE_DEVICES": cumpute_env_value
+#    }
+#c.DockerSpawner.env_keep = ["NVIDIA_VISIBLE_DEVICES"]
+#c.DockerSpawner.environment = {
+#    'NB_USER': "${JUPYTERHUB_USER}", 
+#    'CHOWN_HOME': 'yes',
+#    "NVIDIA_VISIBLE_DEVICES": cumpute_env_value
+#    }
 
 
 
